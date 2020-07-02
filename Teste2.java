393418d84ca872d3fac2839581f78525f7bdaf4c
Hélio Shinohara <38836108+ShinoharaHelioAk@users.noreply.github.com>
2020-07-02 20:45:33 -0300

    Adição de comentário na linha 6.
    Adição de comentário na linha 6.

--- Teste2.java
+++ Teste2.java
@@ -3,5 +3,6 @@ class Teste2 {
     //Adicionando comentário 1 para esta classe...
     public static void main(String[] args) {
         System.out.println("Hello World Teste 2!!!!" + numero);
+        //Comentário adicionado arbitrariamente sem motivo aparente.
     }
 }
