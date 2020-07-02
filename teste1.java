455fea1a2bccdf2eb6975e2bdd57a7e53f294328
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-07-02 14:29:49 -0300

    Inclusão de comentários nas linhas 3 e 4.

--- teste1.java
+++ teste1.java
@@ -1,5 +1,7 @@
 class Teste1 {
     public static void main(String[] args) {
+        //Esqueci de incluir o comentário 1...
+        //Esqueci de incluir o comentário 2...
         System.out.println("Hello World!!!!");
     }
 }
