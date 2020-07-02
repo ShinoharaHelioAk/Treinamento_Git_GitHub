4b3a89dfb5d7c9a279652d07d373d6f070c4dedc
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-07-02 20:28:38 -0300

    Alteração A

--- teste1.java
+++ teste1.java
@@ -3,5 +3,7 @@ class Teste1 {
         //Esqueci de incluir o comentário 1...
         //Esqueci de incluir o comentário 2...
         System.out.println("Hello World!!!!");
+	System.out.println("Linha adicionada para causar conflito!!!!!");
     }
 }
+
