a9db7e39029e65ca5f17cf77fc6d2c777df92f05
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-07-02 21:06:45 -0300

    Restore README.md e Commit Teste2.java

---
+++ README.md
@@ -0,0 +1,4 @@
+# hello-world
+Olá!
+Não sou de falar muito de mim, mas quero aprender muito nessa plataforma de desenvolvimento e versionamento de projetos!
+Apenas isso!!!
--- Teste2.java
+++ Teste2.java
@@ -1,8 +1,5 @@
 class Teste2 {
-    private static Integer numero = 2020;
-    //Adicionando comentário 1 para esta classe...
     public static void main(String[] args) {
-        System.out.println("Hello World Teste 2!!!!" + numero);
-        //Comentário adicionado arbitrariamente sem motivo aparente.
+        System.out.println("Hello World Teste 2!!!!");
     }
 }
